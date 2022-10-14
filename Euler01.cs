a195bc6739dfa7592d079f016f7ebbded6b763b3
jorislimonier <joris.limonier@hotmail.fr>
2022-10-14 09:03:40 +0200

    Add learning material

---
+++ Euler01.cs
@@ -0,0 +1,46 @@
+﻿namespace EulerProblems
+{
+    class Euler
+    {
+        static int One()
+        {
+            int total = 0;
+
+            // Loop until 1000
+            for (int i = 1; i < 1000; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    total += i;
+                };
+            };
+            return total;
+        }
+
+        static int Two()
+        {
+            int res = -1;
+            int nextFib = 0;
+
+            List<int> fibList = new List<int> { 0, 1 };
+            while (nextFib < 400000)
+            {
+                int nbElem = fibList.Count();
+                int penLastFib = fibList.ElementAt(nbElem - 2);
+                int lastFib = fibList.ElementAt(nbElem - 1);
+                nextFib = penLastFib + lastFib;
+                fibList.Add(nextFib);
+                System.Console.WriteLine(lastFib);
+            }
+
+            
+            return res;
+        }
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine(Two());
+        }
+
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Learn C#
+
+This repository contains the material supporting my discovery of the language C#.
